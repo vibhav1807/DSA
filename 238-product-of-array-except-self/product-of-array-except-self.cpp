025a5f793d0f1b842c525cd354ab5eebe5532235
Vibhav <204739953+vibhav1807@users.noreply.github.com>
2026-08-24 23:14:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (60.76%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,15 +4,7 @@ public:
         int n = nums.size();
         vector<int> result(n, 0);
         int count = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == 0){
-                count++;
-            }
-            if(count > 1) break;
-        }
-        if(count > 1){
-            return result;
-        }
+
         int p = 1;
         for(int i = 0; i < n; i++){
             p *= nums[i];
